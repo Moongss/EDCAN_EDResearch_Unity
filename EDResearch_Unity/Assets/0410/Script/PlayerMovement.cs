6b68add5f4282cd9a57b92f8367161b7e37d772d
Moongs <min@adminui-Mac-Pro.local>
2017-05-20 12:56:12 +0900

    EDCAN_EDResearch_1to4

---
+++ EDResearch_Unity/Assets/0410/Script/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	public Rigidbody my_rb;
+	public float speed = 0.5f;
+	public float jumpForce = 1f;
+	// Use this for initialization
+	void Start () {
+		my_rb = this.gameObject.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.A)) {
+			this.gameObject.transform.Translate (new Vector3 (-speed, 0, 0));
+		}
+		if (Input.GetKey(KeyCode.D)) {
+			this.gameObject.transform.Translate (new Vector3 (speed, 0, 0));
+		}
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			my_rb.AddForce (new Vector3 (0, jumpForce, 0));
+		}
+	}
+}
